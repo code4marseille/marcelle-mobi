c44016eb2fb5c34b23ff5145b6cda317caa5c8ad
Xavier Vitali <44967621+xaviervitali@users.noreply.github.com>
2019-09-17 23:10:40 +0200

    condition moyen de transport+page "searching"

--- pages/marius.vue
+++ pages/marius.vue
@@ -1,45 +1,68 @@
 <template>
   <div class="bg-secondary">
-    <div
-      class="d-flex flex-column justify-content-center align-content-center vh-100"
-      v-if="loading"
-      @click="loading=false"
-    >
-      <div>bonjour, je suis marius</div>
-      <p></p>
-      <div>ENSEMBLE ON VA TROUVER LE MEILLEUR MOYEN DE TRANSPORT POUR AÉRER NOTRE VILLE ET SAUVER LES POISSONS</div>
-      <img src="~/assets/images/assistant.svg" alt />
-    </div>
-
-    <div v-else class="d-flex flex-column justify-content-center vh-100">
-      <div class="justify-content-around">
-        <img src="~/assets/images/miniGrandfather.svg" alt />
-        <div class="askBlock">
-          <div class="ask">d'où pars tu ?</div>
-          <input type="text" name="from" id class="inputAsk" v-model="from" @blur="formTo=true" />
-        </div>
+    <div v-if="query">
+      <div
+        class="d-flex flex-column justify-content-center align-content-center vh-100"
+        v-if="loading"
+        @click="loading=false"
+      >
+        <div>bonjour, je suis marius</div>
+        <p></p>
+        <div>ENSEMBLE ON VA TROUVER LE MEILLEUR MOYEN DE TRANSPORT POUR AÉRER NOTRE VILLE ET SAUVER LES POISSONS</div>
+        <img src="~/assets/images/assistant.svg" alt />
       </div>
 
-      <div v-if="formTo" class="block">
-        <img src="~/assets/images/miniGrandfather.svg" alt />
-        <div class="askBlock">
-          <div class="ask">où vas-tu ?</div>
-          <input type="text" name="from" id class="inputAsk" v-model="to" @blur="meansBoolean=true" />
+      <div v-else class="d-flex flex-column justify-content-center vh-100">
+        <div class="justify-content-around">
+          <img src="~/assets/images/miniGrandfather.svg" alt />
+          <div class="askBlock">
+            <div class="ask">d'où pars tu ?</div>
+            <input type="text" name="from" id class="inputAsk" v-model="from" @blur="formTo=true" />
+          </div>
         </div>
-      </div>
-      <div class="rounded">
-        <b-button-group v-if="meansBoolean">
+
+        <div v-if="formTo" class="block">
+          <img src="~/assets/images/miniGrandfather.svg" alt />
+          <div class="askBlock">
+            <div class="ask">où vas-tu ?</div>
+            <input
+              type="text"
+              name="from"
+              id
+              class="inputAsk"
+              v-model="to"
+              @blur="meansBoolean=true"
+            />
+          </div>
+        </div>
+
+        <div class="rounded" v-if="meansBoolean">
+          <h4>Choisissez votre moyen de transport</h4>
+          <b-button-group class="d-flex justify-content-center align-content-center">
+            <b-button
+              type="button"
+              v-for="(mean, idx) in means"
+              :key="idx"
+              :pressed.sync="mean.state"
+              @click="deactive(idx)"
+            >
+              <img :src="require('~/assets/images/' + mean.logo)" :alt="mean.text" />
+            </b-button>
+          </b-button-group>
           <b-button
-            type="button"
-            v-for="(mean, idx) in means"
-            :key="idx"
-            :pressed.sync="mean.state"
-          >
-            <img :src="require('~/assets/images/' + mean.logo)" :alt="mean.text" />
-            <!-- <p class="align-self-end">{{mean.text}}</p> -->
-          </b-button>
-        </b-button-group>
+            class="block"
+            pill
+            variant="primary"
+            id="submit"
+            :disabled="meanSelected===-1"
+            v-click="query=false"
+          >Analyser mon parcours</b-button>
+        </div>
       </div>
+    </div>
+    <div v-else class="d-flex flex-column justify-content-around align-content-center vh-100">
+      <div>laisse moi réflechir</div>
+      <img src="~/assets/images/assistant.svg" alt />
     </div>
   </div>
 </template>
@@ -53,23 +76,38 @@ export default {
       from: "",
       to: "",
       meansBoolean: false,
-      isDeactive: false,
+
       means: [
-        { logo: "rtm.svg.png", text: "Transport en commun", state: true },
-        { logo: "trot.svg", text: "Trotinette", state: true },
-        { logo: "walk.svg", text: "Marche", state: true },
-        { logo: "bike.svg", text: "Vélo", state: true },
-        { logo: "car.svg", text: "Voiture", state: true }
-      ]
+        { logo: "trot.svg", text: "Trotinette", state: false },
+        { logo: "walk.svg", text: "Marche", state: false },
+        { logo: "rtm.svg.png", text: "Transport en commun", state: false },
+        { logo: "bike.svg", text: "Vélo", state: false },
+        { logo: "car.svg", text: "Voiture", state: false }
+      ],
+      meanSelected: -1,
+      query: true
     };
   },
+  computed: {},
   methods: {
-    myFilter: function() {
-      this.i = !this.i;
-      // some code to filter users
+    deactive: function(id) {
+      for (let i = 0; i < this.means.length; i++) {
+        if (id != i) {
+          this.means[i].state = false;
+          // console.log("toto");
+        }
+        this.isSelected();
+        // console.log(this.means.map(mean => mean.state));
+      }
+    },
+
+    isSelected: function() {
+      const activated = this.means.findIndex(function(e) {
+        return e.state === true;
+      });
+      this.meanSelected = activated;
     }
   },
-
   mounted() {
     setTimeout(() => {
       this.loading = false;
@@ -109,6 +147,6 @@ button.active {
 }
 
 .block {
-  margin: 10% 0 10% 0;
+  margin: 5% 0 5% 0;
 }
 </style>
