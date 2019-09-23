cdd91bbce343d71a9551e05a30bf8c82571ca64d
Xavier Vitali <44967621+xaviervitali@users.noreply.github.com>
2019-09-23 17:55:09 +0200

    borne de recharge map

---
+++ pages/chargingmap.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="mapPage">
+    <div id="position">
+      <l-map id="map" :zoom="15" :center="initialLocation" ref="mapd">
+        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+        <ChargingMarker
+          v-for="(charging,i) in $store.state.chargingmap.chargings"
+          :key="'charging'+i"
+          :charging="charging"
+          :select="viewPoint"
+        />
+        <Locatecontrol />
+      </l-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import { LMap, LTileLayer, LControlZoom, LMarker } from "vue2-leaflet";
+import Locatecontrol from "vue2-leaflet-locatecontrol";
+import ChargingMarker from "~/components/ChargingMarker.vue";
+export default {
+  components: {
+    Locatecontrol,
+    LMap,
+    LTileLayer,
+    ChargingMarker
+  },
+  data() {
+    return {
+      initialLocation: [43.295336, 5.373907]
+    };
+  },
+
+  methods: {
+    flyTo(latLng, zoom) {
+      this.$refs.map.mapObject.flyTo(latLng, zoom);
+    }
+  },
+  created() {
+    this.$store.dispatch("chargingmap/fetchCharging");
+  }
+};
+</script>
+
+<style lang="scss">
+#mapPage {
+  .leaflet-left {
+    right: 0 !important;
+    padding-right: 10px;
+    left: unset;
+  }
+
+  #position {
+    position: relative;
+  }
+
+  #map {
+    width: 100wh;
+    height: 100vh;
+  }
+
+  .textFilter {
+    font-size: 0.5rem;
+    margin-bottom: 0;
+  }
+
+  .leaflet-control-attribution {
+    display: none;
+  }
+}
+</style>
+
