497d6fc58267022aed7e79e5493a595f98d8bfc1
Pauline Dussart <paulinedussart04@gmail.com>
2019-09-27 16:39:42 +0200

    add 2 avatoars

--- pages/marius.vue
+++ pages/marius.vue
@@ -4,14 +4,23 @@
       <div
         class="d-flex flex-column justify-content-center align-content-center vh-100 px-3"
         v-if="loading"
-        @click="loading=false"
       >
-        <div>bonjour, je suis marius</div>
+        <div>bonjour a toi !</div>
         <p></p>
         <div
           class="mb-4"
-        >ENSEMBLE ON VA TROUVER LE MEILLEUR MOYEN DE TRANSPORT POUR AÉRER NOTRE VILLE ET SAUVER LES POISSONS</div>
-        <img src="~/assets/images/assistant.svg" alt />
+        >ENSEMBLE NOUS ALLONS TROUVER LE MEILLEUR MOYEN DE TRANSPORT POUR AÉRER NOTRE VILLE ET SAUVER LA PLANETE</div>
+
+        <div>
+          <p>Choisis ton avatar !</p>
+        </div>
+        <div class="d-flex justify-content-between w-100">
+          <div class="marcelle_marius_avatar" v-for="(avatar, i) in avatars" :key="i">
+            <img class="photo_avatar" :src="avatar.icon" />
+            <div class="nom_avatar">{{avatar.name}}</div>
+          </div>
+        </div>
+        <button class="btn-avatar mt-5" @click="loading=false">Valider</button>
       </div>
 
       <div v-else class="d-flex flex-column justify-content-center vh-100">
@@ -98,6 +107,20 @@ export default {
         { logo: 'bike.svg', text: 'Vélo', state: false },
         { logo: 'car.svg', text: 'Voiture', state: false }
       ],
+
+      avatars: [
+        {
+          icon: require('~/assets/images/grandmother.svg'),
+          name: 'MARCELLE',
+          class: 'active'
+        },
+        {
+          icon: require('~/assets/images/grandfather.svg'),
+          name: 'MARIUS',
+          class: ''
+        }
+      ],
+
       meanSelected: -1,
       query: true
     }
@@ -121,12 +144,12 @@ export default {
       })
       this.meanSelected = activated
     }
-  },
-  mounted() {
-    setTimeout(() => {
-      this.loading = false
-    }, 5000)
   }
+  // mounted() {
+  //   setTimeout(() => {
+  //     this.loading = false
+  //   }, 5000)
+  // }
 }
 </script>
 
@@ -168,6 +191,53 @@ export default {
   .icons_search {
     min-height: 25px;
     min-width: 25px;
+  }
+
+  .btn-avatar {
+    padding: 5px 10px;
+    width: 150px;
+    background: #0e5da4;
+    color: white;
+    border-radius: 30px;
+    margin: 0 auto;
+  }
+
+  .marcelle_marius_avatar:hover {
+    background-color: white;
+    width: 160px;
+    height: 160px;
+    border-radius: 50%;
+    margin: 15px;
+    box-shadow: 3px 3px 10px rgba($color: #000, $alpha: 0.5);
+    margin-top: 5px !important;
+    font-size: 16px !important;
+
+    .nom_avatar {
+      color: #0e5da4 !important;
+    }
+    .photo_avatar {
+      width: 120px;
+      margin-top: 5px;
+    }
+  }
+
+  .marcelle_marius_avatar {
+    background-color: rgba($color: #fff, $alpha: 0.2);
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    margin: 15px;
+
+    .nom_avatar {
+      color: white !important;
+      margin-top: 5px;
+      font-size: 13px;
+    }
+
+    .photo_avatar {
+      width: 80px;
+      margin-top: 5px;
+    }
   }
 }
 </style>
