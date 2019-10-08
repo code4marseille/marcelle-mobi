316849c3fa102978740363719def6fd5fa4f6857
Mavrick Munoz <mavrick.munoz@gmail.com>
2019-10-08 16:00:50 +0200

    map added with right style

--- pages/wander.vue
+++ pages/wander.vue
@@ -1,23 +1,30 @@
 <!-- pages/le_nom_de_ma_page.vue => localhost:3000/le_nom_de_ma_page -->
 <template>
-  <div class="text-center pt-5">
-    <h1 id="titre">Caca</h1>
-    <h2>Je suis un example de page en VanillaJS</h2>
-  </div>
+  <div id="map"></div>
 </template>
 <script>
+import * as L from "leaflet";
 export default {
   mounted() {
-    // VOTRE JS ICI
-    const titre = document.getElementById('titre')
-    titre.onclick = () => {
-      alert('Vous avez cliqué')
-    }
+    const map = L.map('map', { zoomControl: false })
+                 .setView([43.295336, 5.373907], 16);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      maxZoom: 18,
+      id: 'streets-v11',
+      accessToken: process.env.MAPBOX_API_KEY
+    }).addTo(map);
+
+    L.control.zoom({
+      position:'topright'
+    }).addTo(map);
   }
 }
 </script>
 <style scoped>
-h1 {
-  color: red;
+#map {
+  width: 100%;
+  height: 100vh;
 }
+
 </style>
