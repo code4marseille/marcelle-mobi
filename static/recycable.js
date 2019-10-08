c90829e9bf937d43c07e0e30aead47e55d78853f
remi bongiovanni <remi.bongiovanni@gmail.com>
2019-10-08 17:39:16 +0200

    seedclothes

---
+++ static/recycable.js
@@ -0,0 +1,193 @@
+export default {
+    clothes: [
+        {
+            title: 'Paroisse St François' ,
+            address: '26 rue(s) Raphaêl Ponson 13008 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.265286,
+            Longitude : 5.385025,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Géant Casino' ,
+            address: '365 avenue de Mazargues 13008 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.260406,
+            Longitude : 5.394218,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Zenatti' ,
+            address: '113 avenue André Zénatti 13008 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.246064,
+            Longitude : 5.392088,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'ERDF' ,
+            address: '76 Traverse de la Gaye 13009 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.258300,
+            Longitude : 5.409183,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Andre Audoli' ,
+            address: '217 rue(s) Andre audoli 13010 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.271813,
+            Longitude : 5.428744,
+            phone: '04 91 67 14 14'
+        },
+         {
+            title: 'Charles Livion' ,
+            address: '121 boulevard Charles Livion 13007 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.291819,
+            Longitude : 5.356016,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Augustin Cieussa' ,
+            address: '15 boulevard Augustin Cieussa 13007 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.287367,
+            Longitude : 5.354047,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Seyssaud' ,
+            address: '3 rue(s) René Seyssaud 13007 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.283210,
+            Longitude : 5.359950,
+            phone: '04 91 67 14 14'
+        },
+         {
+            title: 'Avenue des Roches' ,
+            address: '77 avenue Des Roches 13007 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.279661,
+            Longitude : 5.363675,
+            phone: '04 91 67 14 14'
+        },
+         {
+            title: 'Roucas Blanc' ,
+            address: 'chemin(s) du Roucas Blanc 13007 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.278709,
+            Longitude : 5.367619,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Estrangin' ,
+            address: '101 boulevard Georges Estrangin 13007 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.277099,
+            Longitude : 5.372238,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Breteuil' ,
+            address: '208 rue(s) Breteuil 13006 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.281085,
+            Longitude : 5.378386,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Lacédémone' ,
+            address: '7 rue(s) Lacédémone 13006 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.281888,
+            Longitude : 5.377262,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Notre Dame' ,
+            address: '113 boulevard Notre Dame 13006 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.284397,
+            Longitude : 5.375118,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Tellène' ,
+            address: '112 boulevard Tellène 13007 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.284473,
+            Longitude : 5.367912,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Sauveur Tobelem' ,
+            address: '4 rue(s) Sauveur Tobelem 13007 MARSEILLE',
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.288921,
+            Longitude : 5.366142,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Endoume' ,
+            address: "36 rue(s) d'Endoume 13007 MARSEILLE",
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.289237,
+            Longitude : 5.366272,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Avenue Saint Jean' ,
+            address: "22 avenue Saint Jean 13002 MARSEILLE",
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.296258,
+            Longitude : 5.365890,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Place Jules Verne' ,
+            address: "place Jules Verne 13002 MARSEILLE",
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.296528,
+            Longitude : 5.368640,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Chemin du pont' ,
+            address: "2 chemin(s) du Pont 13002 MARSEILLE",
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.296482,
+            Longitude : 5.369780,
+            phone: '04 91 67 14 14'
+        },
+        {
+            title: 'Mery' ,
+            address: "6 rue(s) Mery 13002 MARSEILLE",
+            Nombre de conteneurs : 1,
+            Etat : 'Disponible',
+            Latitude : 43.298296,
+            Longitude : 5.371157,
+            phone: '04 91 67 14 14'
+        }
+    ]
+}
