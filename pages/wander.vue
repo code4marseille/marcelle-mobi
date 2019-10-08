3508d46f204d64be5406ad9aabce10f4be9b6aa6
Mathias Allagnat <mathiasallagnat@gmail.com>
2019-10-08 17:30:18 +0200

    add-marker function done

--- pages/wander.vue
+++ pages/wander.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="map"></div>
+  <div id="wander">
+    <div id="map"></div>
+  </div>
 </template>
 <script>
 import mapboxgl from "mapbox-gl";
@@ -8,48 +10,56 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 export default {
   mounted() {
-    const markers = [43.270038, 5.395930];
+    mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
 
-    const initMap = () => {
-      mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
-      const map = new mapboxgl.Map({
-        container: "map",
-        style: "mapbox://styles/mapbox/streets-v11",
-        center: [5.373907, 43.295336],
-        zoom: 16,
-        hash: true
-      })
+    const markers = [
+      {
+        coordinates: [5.373907, 43.295336]
+      },
 
-      map.addControl(
-        new MapboxGeocoder({
-          accessToken: mapboxgl.accessToken,
-          marker: false,
-        }),
-      );
+    ];
+
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [5.373907, 43.295336],
+      zoom: 16,
+      hash: true
+    })
+
+    map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        marker: false,
+      }),
+    );
+
+    const addMarkerToMap = (coordinates) => {
+      var el = document.createElement('div');
+      el.className = 'marker';
+
+      new mapboxgl.Marker(el)
+                  .setLngLat(coordinates)
+                  .addTo(map);
     };
 
-    const addMarkerToMap = () => {
-        geojson.features.forEach(function(marker) {
-
-        var el = document.createElement('div');
-        el.className = 'marker';
-
-          // make a marker for each feature and add to the map
-        new mapboxgl.Marker(el)
-        .setLngLat(marker.geometry.coordinates)
-        .addTo(map);
-      });
-    };
-
-    initMap();
-    addMarkerToMap();
+    addMarkerToMap(markers[0].coordinates);
   }
 }
 </script>
-<style scoped>
-#map {
-  width: 100%;
-  height: 100vh;
-}
+<style lang='scss'>
+ #wander {
+    #map {
+      width: 100%;
+      height: 100vh;
+    }
+    .marker {
+      background-image: url('../assets/images/pin.svg');
+      background-size: cover;
+      width: 40px;
+      height: 40px;
+      cursor: pointer;
+    }
+  }
 
 </style>
