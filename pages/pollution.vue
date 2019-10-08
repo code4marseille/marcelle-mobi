8b45cfc602aec247fea010769fe9aedd876134a4
nrcaz <nrcaz@pop-os.localdomain>
2019-10-08 22:15:37 +0200

    feature: map with pollution layer

---
+++ pages/pollution.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="mapPage">
+    <div id="position">
+      <l-map id="map" :zoom="16" :center="location" ref="map">
+        <MapboxTile />
+        <l-wms-tile-layer
+          base-url="http://geoservices.atmosud.org/geoserver/mod_sudpaca_2018/wms?"
+          layers="mod_sudpaca_2018_no2_moyan"
+          format="image/png"
+          layer-type="base"
+          :transparent="true"
+          opacity="0.5"
+        />
+        <LocateControl />
+      </l-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import { LMap, LWMSTileLayer } from 'vue2-leaflet'
+import * as L from 'leaflet'
+import LocateControl from '~/components/LocateControl'
+import MapboxTile from '~/components/MapboxTile.vue'
+
+export default {
+  components: {
+    LMap,
+    LocateControl,
+    'l-wms-tile-layer': LWMSTileLayer,
+    MapboxTile
+  },
+  data() {
+    return {
+      location: { lat: 43.295336, lng: 5.373907 },
+      clusterOptions: {
+        spiderfyOnMaxZoom: false,
+        maxClusterRadius: 40,
+        disableClusteringAtZoom: 17,
+        iconCreateFunction: cluster => {
+          var markers = cluster.getAllChildMarkers()
+          var html = `<div>${markers.length}</div>`
+          return L.divIcon({
+            html: html,
+            className: 'clusterMarker',
+            iconSize: L.point(32, 32)
+          })
+        }
+      }
+    }
+  },
+  created() {
+    this.$store.dispatch('map/fetchAllVehicles', this.location)
+  },
+  methods: {
+    flyTo(latLng, zoom) {
+      this.$refs.map.mapObject.flyTo(latLng, zoom)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#mapPage {
+  .spinnerLoading {
+    z-index: 1000;
+    position: fixed;
+    top: 50vh;
+    left: 50vw;
+  }
+  .leaflet-left {
+    right: 0 !important;
+    padding-right: 10px;
+    left: unset;
+  }
+
+  #position {
+    position: relative;
+    display: flex;
+  }
+
+  #map {
+    width: 100wh;
+    height: 100vh;
+    position: relative;
+    display: flex;
+  }
+
+  .textFilter {
+    font-size: 0.5rem;
+    margin-bottom: 0;
+  }
+
+  .leaflet-control-attribution {
+    display: none;
+  }
+  .clusterMarker {
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background-color: #3498db;
+    color: white;
+    text-align: center;
+    font-size: 20px;
+    line-height: 40px;
+    margin-top: -20px;
+    margin-left: -20px;
+    border: white 3px solid;
+    box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15) !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
+
